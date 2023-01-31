2e3b98fb1446450dbd37c40927a705424fa05f17
eduardofal <edubf136cs@gmail.com>
2023-01-30 23:13:47 -0300

    SysAdicionarProdutos v1.00

---
+++ SysAdicionarProdutos/Produto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace SysAdicionarProdutos
+{
+    internal class Produto
+    {
+        public string Nome {get; set;}
+        public double Preco { get; set;}
+        public int Quantidade { get; set;}
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public double Total()
+        {
+            double total = Quantidade * Preco;
+
+            return total;
+        }
+
+        public override string ToString()
+        {
+            string dados = "Nome: " + Nome + " | Preço: " + Preco.ToString("F2", CultureInfo.InvariantCulture) + " | Quantidade: " + Quantidade + " | Valor total: " + Total().ToString("F2", CultureInfo.InvariantCulture);
+
+            return dados;
+        }
+    }
+}
---
+++ SysAdicionarProdutos/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace SysAdicionarProdutos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+            Console.Write("Quantos produtos deseja adicionar?: ");
+            int quantidadeAdd = int.Parse(Console.ReadLine());
+
+            Produto[] vect = new Produto[quantidadeAdd];
+
+            for (int i = 0; i < quantidadeAdd; i++)
+            {
+                int numero = i + 1;
+                Console.WriteLine("Digite as informações do produto #" + numero);
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Preço: ");
+                double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Quantidade: ");
+                int quantidade = int.Parse(Console.ReadLine());
+
+                vect[i] = new Produto(nome, preco, quantidade);
+            }
+
+            Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+            Console.WriteLine("Dados dos produtos: ");
+            Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+            for (int i = 0; i < quantidadeAdd; i++)
+            {
+                int numero = i + 1;
+                Console.WriteLine();
+                Console.WriteLine("Produto #" + numero + ":");
+                Console.WriteLine(vect[i]);
+                Console.WriteLine();
+            }
+        }
+    }
+}
